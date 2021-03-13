745174614a9373fe7ff8c570f4b0ec13aaea2572
Matheus Enrique <matheus.hpp135@gmail.com>
2021-03-13 10:27:09 -0300

    Validar login

---
+++ src/dao/DaoLogin.java
@@ -0,0 +1,34 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import connection.SingleConnection;
+
+public class DaoLogin {
+	
+	private Connection connection;
+	
+	public DaoLogin() {
+		connection = SingleConnection.getConnection();
+	}
+	
+	public boolean validarLogin(String login, String senha) throws Exception {
+		String sql = "SELECT * FROM usuario WHERE login = ? and senha = ?";
+		
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.setString(1, login);
+		statement.setString(2, senha);
+		
+		ResultSet resultSet = statement.executeQuery();
+		
+		/* Se existir usuário com login e senha passados */
+		if (resultSet.next()) { 
+			return true;
+		}
+		
+		/* Não encontrou usuário */
+		return false;
+	}
+}

c199d5e0f20090b2205292d4ee8999a4dfc1766e
Matheus Enrique <matheus.hpp135@gmail.com>
2021-03-16 14:01:54 -0300

    Comandos sql da criação do banco e das tabelas de usuario

---
+++ src/sql.sql
@@ -0,0 +1,31 @@
+-- change the owner field with the name of owner you want to manage this database
+create database "curso-jsp"
+    with
+    owner = owner_name
+    encoding = UTF8
+    LC_COLLATE = 'pt_BR.UTF-8'
+    LC_CTYPE = 'pt_BR.UTF-8'
+    tablespace = pg_default
+    connection limit = -1
+    template template0;
+
+-- table usuarios
+create table usuario (
+  id bigint primary key,
+  nome character varying(255),
+  login character varying(255),
+  senha character varying(255)
+);
+
+-- sequence for autoincrement id (there are three commands. Run ony by one)
+create sequence serialuser
+  increment 1
+  minvalue 1
+  maxvalue 9223372036854775807
+  start 1
+  cache 1;
+  
+alter table serialuser
+    owner to user_owner;
+  
+alter table usuario alter column id set default nextval('serialuser'::regclass);

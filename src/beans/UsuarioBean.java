e616ba4a909b224ae3e69390db1e26041366a3a7
Matheus Enrique <matheus.hpp135@gmail.com>
2021-03-23 09:14:23 -0300

    Atributos ibge, cep, rua, bairro, cidade, estado adicionados

--- src/beans/UsuarioBean.java
+++ src/beans/UsuarioBean.java
@@ -7,6 +7,12 @@ public class UsuarioBean {
     private String login;
     private String senha;
     private String telefone;
+    private String ibge;
+    private String cep;
+    private String rua;
+    private String bairro;
+    private String cidade;
+    private String estado;
 
     public Long getId() {
         return id;
@@ -39,12 +45,61 @@ public class UsuarioBean {
     public void setSenha(String senha) {
         this.senha = senha;
     }
-    
+
     public String getTelefone() {
         return telefone;
-    }  
+    }
+
     public void setTelefone(String telefone) {
         this.telefone = telefone;
+    }
+
+    public String getIbge() {
+        return ibge;
+    }
+
+    public void setIbge(String ibge) {
+        this.ibge = ibge;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
     }
 
 }

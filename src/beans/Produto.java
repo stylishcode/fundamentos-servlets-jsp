41daf04bb93558b655a020d05fe32d057d78e68c
Matheus Enrique <matheus.hpp135@gmail.com>
2021-03-23 09:20:15 -0300

    Modelo de produto

---
+++ src/beans/Produto.java
@@ -0,0 +1,44 @@
+package beans;
+
+import java.math.BigDecimal;
+
+public class Produto {
+
+    private Long id;
+    private String nome;
+    private int quantidade;
+    private BigDecimal valor;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+}

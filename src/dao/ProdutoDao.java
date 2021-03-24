328338de9e02af46a8d4d814ab2b7f52e00e79df
Matheus Enrique <matheus.hpp135@gmail.com>
2021-03-24 17:36:04 -0300

    Refatoração de alguns métodos. Método de verificação de produto duplicado na inserção e na alteração

--- src/dao/ProdutoDao.java
+++ src/dao/ProdutoDao.java
@@ -37,36 +37,36 @@ public class ProdutoDao {
                 e1.printStackTrace();
             }
         }
-        
+
     }
-    
+
     public List<Produto> listar() throws SQLException {
         List<Produto> produtos = new ArrayList<Produto>();
-        
+
         String sql = "SELECT * FROM produto";
-        
+
         PreparedStatement pst = connection.prepareStatement(sql);
         ResultSet resultSet = pst.executeQuery();
-        
-        while(resultSet.next()) {
+
+        while (resultSet.next()) {
             Produto produto = new Produto();
             produto.setId(resultSet.getLong(("id")));
             produto.setNome(resultSet.getString(("nome")));
             produto.setQuantidade(resultSet.getInt(("quantidade")));
             produto.setValor(resultSet.getBigDecimal(("valor")));
-            
+
             produtos.add(produto);
         }
-        
+
         resultSet.close();
-        
+
         return produtos;
     }
-    
+
     public void atualizar(Produto produto) {
         try {
             String sql = "UPDATE produto SET nome = ?, quantidade = ?, valor = ? WHERE id = ?";
-            
+
             PreparedStatement pst = connection.prepareStatement(sql);
             pst.setString(1, produto.getNome());
             pst.setInt(2, produto.getQuantidade());
@@ -76,18 +76,23 @@ public class ProdutoDao {
             connection.commit();
         } catch (Exception e) {
             e.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
         }
     }
-    
+
     public void delete(Long id) {
         try {
             String sql = "DELETE FROM produto WHERE id = ?";
-            
+
             PreparedStatement pst = connection.prepareStatement(sql);
             pst.setLong(1, id);
             pst.execute();
             connection.commit();
-            
+
         } catch (Exception e) {
             e.printStackTrace();
             try {
@@ -97,27 +102,68 @@ public class ProdutoDao {
             }
         }
     }
-    
+
     public Produto consultar(Long id) throws SQLException {
         String sql = "SELECT * FROM produto WHERE id = ?";
-        
+
         PreparedStatement pst = connection.prepareStatement(sql);
         pst.setLong(1, id);
-        
+
         ResultSet resultSet = pst.executeQuery();
-        
-        while (resultSet.next()) {
+
+        if (resultSet.next()) {
             Produto produto = new Produto();
             produto.setId(resultSet.getLong("id"));
             produto.setNome(resultSet.getString("nome"));
             produto.setQuantidade(resultSet.getInt("quantidade"));
             produto.setValor(resultSet.getBigDecimal("valor"));
-            
+
             return produto;
         }
-        
+
         resultSet.close();
         return null;
     }
-    
+
+    public boolean isExisteProduto(String nome) {
+        try {
+            String sql = "SELECT COUNT(1) AS qtde FROM produto WHERE nome = ?";
+
+            PreparedStatement pst = connection.prepareStatement(sql);
+            pst.setString(1, nome);
+
+            ResultSet resultSet = pst.executeQuery();
+
+            if (resultSet.next()) {
+                return resultSet.getInt("qtde") > 0;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    public boolean isExisteProdutoOnUpdate(String nome, Long id) {
+        try {
+            String sql = "SELECT COUNT(1) AS qtde FROM produto WHERE nome = ? and id <> ?";
+
+            PreparedStatement pst = connection.prepareStatement(sql);
+            pst.setString(1, nome);
+            pst.setLong(2, id);
+
+            ResultSet resultSet = pst.executeQuery();
+
+            if (resultSet.next()) {
+                return resultSet.getInt("qtde") > 0;
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return false;
+    }
+
 }
